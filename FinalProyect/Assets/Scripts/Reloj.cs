af9e81663e1637c2c5e01bd556824638759fbfa9
rodrigomoreno <morenorodrigo541@gmail.com>
2020-02-05 23:15:03 -0600

    Rodrigo
    Animations y HUD

---
+++ FinalProyect/Assets/Scripts/Reloj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class Reloj : MonoBehaviour
+{
+    public float startTime = 90f;
+    public TextMeshProUGUI ContadorText;
+    public int timeToText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startTime -= Time.deltaTime;
+        timeToText = (int)startTime;
+        ContadorText.text = timeToText.ToString();
+    }
+}
