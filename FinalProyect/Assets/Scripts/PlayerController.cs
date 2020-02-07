4da2ef1a01fdc2dd5caae27717cc2abad8534d9d
rodrigomoreno <morenorodrigo541@gmail.com>
2020-02-06 23:09:36 -0600

    Rodrigo
    PlayerController

--- FinalProyect/Assets/Scripts/PlayerController.cs
+++ FinalProyect/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float jumpForce;
+    public Animator player;
+    public bool isOnTheGround;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.D))
+            player.SetTrigger("Speed");
         
+        if (Input.GetKeyDown(KeyCode.I))
+            player.SetTrigger("Punch");
+
+        if (Input.GetKeyDown(KeyCode.J))
+            player.SetTrigger("KickR");
+
+        if (Input.GetKeyDown(KeyCode.K))
+            player.SetTrigger("KickL");
+
+        if (Input.GetKeyDown(KeyCode.U))
+            player.SetTrigger("Jab");
+
+        if (Input.GetKeyDown(KeyCode.A))
+            player.SetTrigger("SpeedB");
+
     }
 }
