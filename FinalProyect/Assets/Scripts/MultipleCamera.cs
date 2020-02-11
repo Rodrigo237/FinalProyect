13e247993dcda6b50516781abe8f833ca9b26d29
rodrigomoreno <morenorodrigo541@gmail.com>
2020-02-11 01:54:39 -0600

    Rodrigo
    Camera Follow and Selection Character

--- FinalProyect/Assets/Scripts/MultipleCamera.cs
+++ FinalProyect/Assets/Scripts/MultipleCamera.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class MultipleCamera : MonoBehaviour
 {
     public List<Transform> targets;
@@ -10,15 +11,47 @@ public class MultipleCamera : MonoBehaviour
     public float smoothTime = 0.5f;
     private Vector3 velocity;
     // Update is called once per frame
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+    private Camera cam;
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
     void LateUpdate()
     {
         if (targets.Count == 0)
             return;
 
+        Move();
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom,minZoom,GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+    void Move()
+    {
+
         Vector3 centerPoint = GetCenterPoint();
         Vector3 newPosition = centerPoint + offset;
 
-        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity,smoothTime); 
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i =0; i < targets.Count;i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
     }
 
     Vector3 GetCenterPoint()
---
+++ FinalProyect/Assets/Scripts/SelectionCharacterManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SelectionCharacterManager : MonoBehaviour
+{
+    public static bool pulgaSelection;
+    public static bool comandanteSelection;
+    public GameObject pulga;
+    public GameObject comandante;
+
+    private void Awake()
+    {
+        pulgaSelection = false;
+        comandanteSelection = false;
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+    public  void SelectionPulga()
+    {
+       
+        pulgaSelection =  true;
+        if (pulgaSelection == true)
+        {
+            pulga.transform.tag = "Player";
+            pulga.GetComponent<PlayerController>().enabled = true;
+            pulga.GetComponent<EnemyController>().enabled = false;
+            comandante.transform.tag = "Enemy";
+            comandante.GetComponent<PlayerController>().enabled = false;
+            comandante.GetComponent<EnemyController>().enabled = true;
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void SelectionCR()
+    {
+
+        comandanteSelection = true;
+        if (comandanteSelection == true)
+        {
+            comandante.transform.tag = "Player";
+            comandante.GetComponent<PlayerController>().enabled = true;
+            comandante.GetComponent<EnemyController>().enabled = false;
+            pulga.transform.tag = "Enemy";
+            pulga.GetComponent<PlayerController>().enabled = false;
+            pulga.GetComponent<EnemyController>().enabled = true;
+            SceneManager.LoadScene(1);
+        }
+      
+        
+    }
+}
